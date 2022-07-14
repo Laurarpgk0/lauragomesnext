998b854d715bb21589bd4c6f12f9bc3b1be385ac
Laurarpgk0 <laurazt6@gmail.com>
2022-07-14 17:57:33 -0300

    EP01

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import useAuth from './../hooks/useAuth';
 
 export default function Home() {
+
+  const { user, signin } = useAuth();
+  console.log('user', user)
+
   return (
     <div>
       <h1>Aprenda programação direto ao ponto 100% free.</h1>
+      <button onClick={() => signin()}>Entrar com github</button>
     </div>
   )
 }
